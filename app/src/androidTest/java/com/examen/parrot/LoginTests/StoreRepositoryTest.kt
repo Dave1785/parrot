74e3c1e8fa0bb7d8151964404852bf8699b17b3d
lenovo <eldark17@gmail.com>
2020-09-16 20:43:51 -0500

    testing stores

--- app/src/androidTest/java/com/examen/parrot/LoginTests/StoreRepositoryTest.kt
+++ app/src/androidTest/java/com/examen/parrot/LoginTests/StoreRepositoryTest.kt
@@ -1,11 +1,14 @@
 package com.examen.parrot.LoginTests
 
+import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.examen.parrot.login.domain.LoginRequestDTO
 import kotlinx.coroutines.test.runBlockingTest
 import org.junit.Before
 import org.junit.Test
 import org.junit.Assert
+import org.junit.runner.RunWith
 
+@RunWith(AndroidJUnit4::class)
 class StoreRepositoryTest {
 
     private lateinit var fakeLoginSource: FakeLoginSource
